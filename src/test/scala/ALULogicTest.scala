0f9e49aee54a5f6b67366b8b9488fb41c1d7707e
谭源 <kentertan12138@Outlook.com>
2021-05-28 09:54:17 +0800

    Write a function which makes using SInt in ChiselTest become possible Finish ALU_LT ALU_NE test

--- src/test/scala/ALULogicTest.scala
+++ src/test/scala/ALULogicTest.scala
@@ -4,6 +4,13 @@ import chiseltest._
 import chisel3._
 
 class ALULogicTest extends FlatSpec with ChiselScalatestTester with Matchers {
+
+  /**
+   * @param x A positive number which should be a negative number
+   * @return SInt.asUInt()
+   */
+  private def ToNegativeSInt(x: Int): Int = scala.math.pow(2, 32).toInt - x
+
   behavior of "ibex_alu"
   it should "do ALU_ADD" in {
     test(new ibex_alu) { c =>
@@ -42,6 +49,50 @@ class ALULogicTest extends FlatSpec with ChiselScalatestTester with Matchers {
       c.io.operand_b_i.poke(1.U)
       c.io.multdiv_sel_i.poke(false.B)
       c.io.result_o.expect(4294967295L.U)
+    }
+  }
+  it should "do ALU_LT" in {
+    test(new ibex_alu) { c =>
+      c.io.operator_i.poke(alu_op_e.ALU_LT)
+      c.io.operand_a_i.poke(10.U)
+      c.io.operand_b_i.poke(9.U)
+      c.io.multdiv_sel_i.poke(false.B)
+      c.io.result_o.expect(0.U)
+    }
+    test(new ibex_alu) { c =>
+      c.io.operator_i.poke(alu_op_e.ALU_LT)
+      c.io.operand_a_i.poke(8.U)
+      c.io.operand_b_i.poke(9.U)
+      c.io.multdiv_sel_i.poke(false.B)
+      c.io.result_o.expect(1.U)
+    }
+    test(new ibex_alu) { c =>
+      c.io.operator_i.poke(alu_op_e.ALU_LT)
+      c.io.operand_a_i.poke(8.U)
+      c.io.operand_b_i.poke(8.U)
+      c.io.multdiv_sel_i.poke(false.B)
+      c.io.result_o.expect(0.U)
+    }
+    test(new ibex_alu) { c =>
+      c.io.operator_i.poke(alu_op_e.ALU_LT)
+      c.io.operand_a_i.poke(ToNegativeSInt(8).U)
+      c.io.operand_b_i.poke(ToNegativeSInt(7).U)
+      c.io.multdiv_sel_i.poke(false.B)
+      c.io.result_o.expect(1.U)
+    }
+    test(new ibex_alu) { c =>
+      c.io.operator_i.poke(alu_op_e.ALU_LT)
+      c.io.operand_a_i.poke(ToNegativeSInt(8).U)
+      c.io.operand_b_i.poke(ToNegativeSInt(9).U)
+      c.io.multdiv_sel_i.poke(false.B)
+      c.io.result_o.expect(0.U)
+    }
+    test(new ibex_alu) { c =>
+      c.io.operator_i.poke(alu_op_e.ALU_LT)
+      c.io.operand_a_i.poke(ToNegativeSInt(8).U)
+      c.io.operand_b_i.poke(ToNegativeSInt(8).U)
+      c.io.multdiv_sel_i.poke(false.B)
+      c.io.result_o.expect(0.U)
     }
   }
   it should "do ALU_LTU" in {
@@ -104,6 +155,22 @@ class ALULogicTest extends FlatSpec with ChiselScalatestTester with Matchers {
       c.io.operand_b_i.poke(9.U)
       c.io.multdiv_sel_i.poke(false.B)
       c.io.result_o.expect(0.U)
+    }
+  }
+  it should "do ALU_NE" in {
+    test(new ibex_alu) { c =>
+      c.io.operator_i.poke(alu_op_e.ALU_NE)
+      c.io.operand_a_i.poke(10.U)
+      c.io.operand_b_i.poke(10.U)
+      c.io.multdiv_sel_i.poke(false.B)
+      c.io.result_o.expect(0.U)
+    }
+    test(new ibex_alu) { c =>
+      c.io.operator_i.poke(alu_op_e.ALU_NE)
+      c.io.operand_a_i.poke(10.U)
+      c.io.operand_b_i.poke(9.U)
+      c.io.multdiv_sel_i.poke(false.B)
+      c.io.result_o.expect(1.U)
     }
   }
 }
