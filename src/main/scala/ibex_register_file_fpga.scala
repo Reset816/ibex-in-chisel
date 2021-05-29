a2aaa0f38bcd95a7223de131c3291898ebee08a0
谭源 <kentertan12138@Outlook.com>
2021-05-29 19:45:34 +0800

    Register(fpga) pass test

--- src/main/scala/ibex_register_file_fpga.scala
+++ src/main/scala/ibex_register_file_fpga.scala
@@ -1,38 +1,60 @@
 import chisel3._
+import chisel3.util.{Cat, Fill, MuxLookup, is, switch}
 
-class ibex_register_file_fpga extends Module {
+class ibex_register_file_fpga(
+                               val RV32E: Bool = false.B,
+                               val DataWidth: UInt = 32.U,
+                               val DummyInstructions: Bool = false.B,
+                             ) extends Module {
   val io = IO(new Bundle {
     //Clock and Reset
-    val clk_i:UInt=Input(UInt(1.W))
-    val rst_ni:UInt=Input(UInt(1.W))
-    val test_en_i:UInt=Input(UInt(1.W))
-    val dummy_instr_id_i:UInt=Input(UInt(1.W))
+    val clk_i: Clock = Input(Clock())
+    val rst_ni: UInt = Input(UInt(1.W))
+
+    val test_en_i: UInt = Input(UInt(1.W))
+    val dummy_instr_id_i: UInt = Input(UInt(1.W))
 
     //Read port R1
-    val raddr_a_i:UInt=Input(UInt(5.W))
-    val rdata_a_o:UInt=Output(UInt(32.W))
+    val raddr_a_i: UInt = Input(UInt(5.W))
+    val rdata_a_o: UInt = Output(UInt(32.W))
     //Read port R2
-    val raddr_b_i:UInt=Input(UInt(5.W))
-    val rdata_b_o:UInt=Output(UInt(32.W))
+    val raddr_b_i: UInt = Input(UInt(5.W))
+    val rdata_b_o: UInt = Output(UInt(32.W))
     //Write port W1
-    val waddr_a_i:UInt=Input(UInt(5.W))
-    val wdata_a_i:UInt=Input(UInt(32.W))
-    val we_a_i:UInt=Input(UInt(1.W))
+    val waddr_a_i: UInt = Input(UInt(5.W))
+    val wdata_a_i: UInt = Input(UInt(32.W))
+    val we_a_i: UInt = Input(UInt(1.W))
   })
-    val ADDR_WIDTH:UInt=5.U(1.W)
-    val NUM_WORDS:UInt=
 
-    var mem[NUM_WORDS]=RegInit(0.U(32.W))
-    var we=Wire(UInt(1.W))
+  val ADDR_WIDTH: UInt = 5.U
+  val NUM_WORDS: UInt = 32.U
 
-    //async_read a
-    rdata_a_o:=(io.raddr_a_i==0)?0:mem[io.raddr_a_i]
-    //async_read a
-    rdata_b_o:=(io.raddr_b_i==0)?0:mem[io.raddr_b_i]
-    //we select
-    we:=(io.waddr_a_i)?0:io.we_a_i
+  val mem: Vec[UInt] = Reg(Vec(32, Bits(32.W)))
+  val we = Wire(UInt(1.W))
 
-    if(we)
-      mem[io.waddr_a_i]:=io.wdata_a_i
+  //async_read a
+  io.rdata_a_o := Mux(io.raddr_a_i === 0.U, 0.U, mem(io.raddr_a_i))
+
+  //async_read b
+  io.rdata_b_o := Mux(io.raddr_b_i === 0.U, 0.U, mem(io.raddr_b_i))
+
+  //we select
+  we := Mux(io.waddr_a_i === 0.U, 0.U, io.we_a_i)
+  withClock(io.clk_i) {
+    when(we === true.B) {
+      mem(io.waddr_a_i) := io.wdata_a_i
+    }
+  }
+
+  // Reset not used in this register file version
+  val unused_rst_ni: Bool = Wire(Bool())
+  unused_rst_ni := io.rst_ni
+
+  // Dummy instruction changes not relevant for FPGA implementation
+  val unused_dummy_instr: Bool = Wire(Bool())
+  unused_dummy_instr := io.dummy_instr_id_i
+  // Test enable signal not used in FPGA implementation
+  val unused_test_en: Bool = Wire(Bool())
+  unused_test_en := io.test_en_i
 }
 
---
+++ src/test/scala/RegisterLogicTest.scala
@@ -0,0 +1,21 @@
+import chisel3._
+import chiseltest._
+import org.scalatest._
+
+class RegisterLogicTest extends FlatSpec with ChiselScalatestTester with Matchers {
+
+  behavior of "Store and Load"
+  it should "do Load 3.U in address 2" in {
+    test(new ibex_register_file_fpga) { c =>
+      c.io.raddr_a_i.poke(2.U) // 读2位置的值
+      c.io.rdata_a_o.expect(0.U) //2位置期望得到0.U
+      c.io.we_a_i.poke(1.U)
+      c.io.waddr_a_i.poke(2.U) // 2位置为写入地址
+      c.io.wdata_a_i.poke(3.U) // 向写入地址写入3.U
+
+      c.clock.step()
+      c.io.rdata_a_o.expect(3.U) //2位置期望得到3.U
+    }
+  }
+
+}
