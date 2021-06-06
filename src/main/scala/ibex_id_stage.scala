dcc5b9d9b5a3fe673c2257f40f0548e22d82db4a
谭源 <kentertan12138@Outlook.com>
2021-06-06 11:06:58 +0800

    FUCK SHITHOLE

--- src/main/scala/ibex_id_stage.scala
+++ src/main/scala/ibex_id_stage.scala
@@ -6,93 +6,100 @@ import chisel3.experimental.ChiselEnum
 
 class ibex_id_stage extends Module {
   val io = IO(new Bundle {
-    val ctrl_busy_o = Input(Bool())
+    val ctrl_busy_o: Bool = Input(Bool())
 
-    val instr_valid_i = Input(Bool())
-    val instr_rdata_i = Input(Bool())
-    val instr_rdata_alu_i = Input(Bool())
-    val instr_req_o = Output(Bool())
-    val instr_first_cycle_id_o = Output(Bool())
-    val instr_valid_clear_o = Output(Bool())
-    val id_in_ready_o = Output(Bool())
+    val instr_valid_i: Bool = Input(Bool())
+    val instr_rdata_i: Bool = Input(Bool())
+    val instr_rdata_alu_i: Bool = Input(Bool())
+    val instr_req_o: Bool = Output(Bool())
+    val instr_first_cycle_id_o: Bool = Output(Bool())
+    val instr_valid_clear_o: Bool = Output(Bool())
+    val id_in_ready_o: Bool = Output(Bool())
 
-    val branch_decision_i = Input(Bool())
+    val branch_decision_i: Bool = Input(Bool())
 
-    val pc_set_o = Output(Bool())
-    val pc_mux_o = Output(pc_sel_e())
+    val pc_set_o: Bool = Output(Bool())
+    val pc_mux_o: pc_sel_e.Type = Output(pc_sel_e())
 
-    val pc_id_i = Input(UInt(32.W))
+    val pc_id_i: UInt = Input(UInt(32.W))
 
-    val ex_valid_i = Input(Bool()) // todo: ALU 输出(总是为 1)
+    val ex_valid_i: Bool = Input(Bool()) // todo: ALU 输出(总是为 1)
 
-    val lsu_resp_valid_i = Input(Bool()) // todo: LSU 空闲标志
-    val lsu_req_o = Output(Bool())
-    val lsu_we_o = Output(Bool())
-    val lsu_wdata_o = Output(UInt(32.W))
+    val lsu_resp_valid_i: Bool = Input(Bool()) // todo: LSU 空闲标志
+    val lsu_req_o: Bool = Output(Bool())
+    val lsu_we_o: Bool = Output(Bool())
+    val lsu_wdata_o: UInt = Output(UInt(32.W))
 
-    val alu_operator_ex_o = Output(alu_op_e())
-    val alu_operand_a_ex_o = Output(UInt(32.W))
-    val alu_operand_b_ex_o = Output(UInt(32.W))
+    val alu_operator_ex_o: alu_op_e.Type = Output(alu_op_e())
+    val alu_operand_a_ex_o: UInt = Output(UInt(32.W))
+    val alu_operand_b_ex_o: UInt = Output(UInt(32.W))
 
-    val rf_raddr_a_o = Output(UInt(5.W))
-    val rf_rdata_a_i = Input(UInt(32.W))
-    val rf_raddr_b_o = Output(UInt(5.W))
-    val rf_rdata_b_i = Input(UInt(32.W))
-    val rf_ren_a_o = Output(Bool())
-    val rf_ren_b_o = Output(Bool())
+    val rf_raddr_a_o: UInt = Output(UInt(5.W))
+    val rf_rdata_a_i: UInt = Input(UInt(32.W))
+    val rf_raddr_b_o: UInt = Output(UInt(5.W))
+    val rf_rdata_b_i: UInt = Input(UInt(32.W))
+    val rf_ren_a_o: Bool = Output(Bool())
+    val rf_ren_b_o: Bool = Output(Bool())
 
-    val instr_id_done_o = Output(Bool())
+    val rf_waddr_id_o: UInt = Output(UInt(4.W))
+    val rf_we_id_o: Bool = Output(Bool())
+
+
+    val instr_id_done_o: Bool = Output(Bool())
 
 
   })
 
-  val branch_in_dec = Wire(Bool())
+  val branch_in_dec: Bool = Wire(Bool())
 
-  val branch_set = Wire(Bool())
-  val branch_set_raw = Wire(Bool())
-  val branch_set_raw_d = Wire(Bool())
-  val branch_set_raw_q = Wire(Bool())
+  val branch_set: Bool = Wire(Bool())
+  val branch_set_raw: Bool = Wire(Bool())
+  val branch_set_raw_d: Bool = Wire(Bool())
+  val branch_set_raw_q: Bool = Wire(Bool())
 
-  val branch_jump_set_done_q = Wire(Bool())
-  val branch_jump_set_done_d = Wire(Bool())
+  val branch_jump_set_done_q: Bool = Wire(Bool())
+  val branch_jump_set_done_d: Bool = Wire(Bool())
 
-  val branch_taken = Wire(Bool())
+  val branch_taken: Bool = Wire(Bool())
 
-  val jump_in_dec = Wire(Bool())
+  val jump_in_dec: Bool = Wire(Bool())
 
-  val jump_set_dec = Wire(Bool())
-  val jump_set = Wire(Bool())
-  val jump_set_raw = Wire(Bool())
+  val jump_set_dec: Bool = Wire(Bool())
+  val jump_set: Bool = Wire(Bool())
+  val jump_set_raw: Bool = Wire(Bool())
 
-  val instr_first_cycle = Wire(Bool())
-  val instr_executing = Wire(Bool())
-  val instr_done = Wire(Bool())
-  val controller_run = Wire(Bool())
-  val stall_id_hz = Wire(Bool())
-  val stall_mem = Wire(Bool())
-  val stall_branch = Wire(Bool())
-  val stall_jump = Wire(Bool())
-  val stall_id = Wire(Bool())
-  val multicycle_done = Wire(Bool())
+  val instr_first_cycle: Bool = Wire(Bool())
+  val instr_executing: Bool = Wire(Bool())
+  val instr_done: Bool = Wire(Bool())
+  val controller_run: Bool = Wire(Bool())
+  val stall_id_hz: Bool = Wire(Bool())
+  val stall_mem: Bool = Wire(Bool())
+  val stall_branch: Bool = Wire(Bool())
+  val stall_jump: Bool = Wire(Bool())
+  val stall_id: Bool = Wire(Bool())
+  val multicycle_done: Bool = Wire(Bool())
 
-  val imm_i_type = Wire(UInt(32.W))
-  val imm_s_type = Wire(UInt(32.W))
-  val imm_b_type = Wire(UInt(32.W))
-  val imm_u_type = Wire(UInt(32.W))
-  val imm_j_type = Wire(UInt(32.W))
-  val zimm_rs1_type = Wire(UInt(32.W))
+  val imm_i_type: UInt = Wire(UInt(32.W))
+  val imm_s_type: UInt = Wire(UInt(32.W))
+  val imm_b_type: UInt = Wire(UInt(32.W))
+  val imm_u_type: UInt = Wire(UInt(32.W))
+  val imm_j_type: UInt = Wire(UInt(32.W))
+  val zimm_rs1_type: UInt = Wire(UInt(32.W))
 
-  val imm_a = Wire(UInt(32.W))
-  val imm_b = Wire(UInt(32.W))
+  val imm_a: UInt = Wire(UInt(32.W))
+  val imm_b: UInt = Wire(UInt(32.W))
 
-  val rf_we_dec = Wire(Bool())
-  val rf_we_raw = Wire(Bool())
+  // Register file interface
 
-  val rf_ren_a = Wire(Bool())
-  val rf_ren_b = Wire(Bool())
+  val rf_wdata_sel: rf_wd_sel_e.Type = Wire(rf_wd_sel_e())
+  val rf_we_dec: Bool = Wire(Bool())
+  val rf_we_raw: Bool = Wire(Bool())
 
-  val rf_ren_a_dec = Wire(Bool())
-  val rf_ren_b_dec = Wire(Bool())
+  val rf_ren_a: Bool = Wire(Bool())
+  val rf_ren_b: Bool = Wire(Bool())
+
+  val rf_ren_a_dec: Bool = Wire(Bool())
+  val rf_ren_b_dec: Bool = Wire(Bool())
 
   rf_ren_a := io.instr_valid_i & rf_ren_a_dec
   rf_ren_b := io.instr_valid_i & rf_ren_b_dec
@@ -100,40 +107,41 @@ class ibex_id_stage extends Module {
   io.rf_ren_a_o := rf_ren_a
   io.rf_ren_b_o := rf_ren_b
 
-  val rf_rdata_a_fwd = Wire(UInt(32.W))
-  val rf_rdata_b_fwd = Wire(UInt(32.W))
+  val rf_rdata_a_fwd: UInt = Wire(UInt(32.W))
+  val rf_rdata_b_fwd: UInt = Wire(UInt(32.W))
 
-  val alu_operator = Wire(alu_op_e())
-  val alu_op_a_mux_sel = Wire(op_a_sel_e())
-  val alu_op_a_mux_sel_dec = Wire(op_a_sel_e())
-  val alu_op_b_mux_sel = Wire(op_b_sel_e())
-  val alu_op_b_mux_sel_dec = Wire(op_b_sel_e())
-  val alu_multicycle_dec = Wire(Bool())
-  val stall_alu = Wire(Bool())
-  val imm_a_mux_sel = Wire(imm_a_sel_e())
-  val imm_b_mux_sel = Wire(imm_b_sel_e())
+  val alu_operator: alu_op_e.Type = Wire(alu_op_e())
+  val alu_op_a_mux_sel: op_a_sel_e.Type = Wire(op_a_sel_e())
+  val alu_op_a_mux_sel_dec: op_a_sel_e.Type = Wire(op_a_sel_e())
+  val alu_op_b_mux_sel: op_b_sel_e.Type = Wire(op_b_sel_e())
+  val alu_op_b_mux_sel_dec: op_b_sel_e.Type = Wire(op_b_sel_e())
+  val alu_multicycle_dec: Bool = Wire(Bool())
+  val stall_alu: Bool = Wire(Bool())
+  val imm_a_mux_sel: imm_a_sel_e.Type = Wire(imm_a_sel_e())
+  val imm_b_mux_sel: imm_b_sel_e.Type = Wire(imm_b_sel_e())
+  val imm_b_mux_sel_dec: imm_b_sel_e.Type = Wire(imm_b_sel_e())
 
 
   // TODO: Data Memory Control
-  val lsu_we = Wire(Bool())
-  val lsu_req = Wire(Bool())
-  val lsu_req_dec = Wire(Bool())
-  val data_req_allowed = Wire(Bool())
+  val lsu_we: Bool = Wire(Bool())
+  val lsu_req: Bool = Wire(Bool())
+  val lsu_req_dec: Bool = Wire(Bool())
+  val data_req_allowed: Bool = Wire(Bool())
 
-  val alu_operand_a = Wire(UInt(32.W))
-  val alu_operand_b = Wire(UInt(32.W))
+  val alu_operand_a: UInt = Wire(UInt(32.W))
+  val alu_operand_b: UInt = Wire(UInt(32.W))
 
   imm_a := Mux(imm_a_mux_sel === imm_a_sel_e.IMM_A_Z, zimm_rs1_type, "0".U)
 
-  alu_operand_a := MuxLookup(alu_op_a_mux_sel,io.pc_id_i,Array(
+  alu_operand_a := MuxLookup(alu_op_a_mux_sel, io.pc_id_i, Array(
     op_a_sel_e.OP_A_REG_A.asUInt() -> rf_rdata_a_fwd,
     // todo: 不支持异常，不可能进入这种情况
     // op_a_sel_e.OP_A_FWD.asUInt() -> io.lsu_addr_last_i,
     op_a_sel_e.OP_A_CURRPC.asUInt() -> io.pc_id_i,
     op_a_sel_e.OP_A_IMM.asUInt() -> imm_a,
-    ))
+  ))
 
-  imm_b := MuxLookup(imm_b_mux_sel,4.U(32.W),Array(
+  imm_b := MuxLookup(imm_b_mux_sel, 4.U(32.W), Array(
     imm_b_sel_e.IMM_B_I.asUInt() -> imm_i_type,
     imm_b_sel_e.IMM_B_S.asUInt() -> imm_s_type,
     imm_b_sel_e.IMM_B_B.asUInt() -> imm_b_type,
@@ -141,7 +149,7 @@ class ibex_id_stage extends Module {
     imm_b_sel_e.IMM_B_J.asUInt() -> imm_j_type,
     imm_b_sel_e.IMM_B_INCR_PC.asUInt() -> 4.U(32.W),
     imm_b_sel_e.IMM_B_INCR_ADDR.asUInt() -> 4.U(32.W),
-    ))
+  ))
 
   alu_operand_b := Mux(alu_op_b_mux_sel === op_b_sel_e.OP_B_IMM, imm_b, rf_rdata_b_fwd)
   io.rf_we_id_o := rf_we_raw & instr_executing
@@ -151,8 +159,8 @@ class ibex_id_stage extends Module {
   jump_set_dec := decoder.io.jump_set_o
   decoder.io.branch_taken_i := true.B // branch_taken_i 永远为true
   decoder.io.instr_first_cycle_i := instr_first_cycle
-  decoder.io.instr_rdata_i := instr_rdata_i
-  decoder.io.instr_rdata_alu_i := instr_rdata_alu_i
+  decoder.io.instr_rdata_i := io.instr_rdata_i
+  decoder.io.instr_rdata_alu_i := io.instr_rdata_alu_i
 
   imm_a_mux_sel := decoder.io.imm_a_mux_sel_o
   imm_b_mux_sel_dec := decoder.io.imm_b_mux_sel_o
@@ -165,9 +173,9 @@ class ibex_id_stage extends Module {
 
   rf_wdata_sel := decoder.io.rf_wdata_sel_o
   rf_we_dec := decoder.io.rf_we_o
-  rf_raddr_a_o := decoder.io.rf_raddr_a_o
-  rf_raddr_b_o := decoder.io.rf_raddr_b_o
-  rf_waddr_id_o := decoder.io.rf_waddr_o
+  io.rf_raddr_a_o := decoder.io.rf_raddr_a_o
+  io.rf_raddr_b_o := decoder.io.rf_raddr_b_o
+  io.rf_waddr_id_o := decoder.io.rf_waddr_o
   rf_ren_a_dec := decoder.io.rf_ren_a_o
   rf_ren_b_dec := decoder.io.rf_ren_b_o
 
@@ -180,19 +188,15 @@ class ibex_id_stage extends Module {
   lsu_we := decoder.io.data_we_o
   // 本项目 LSU 仅支持 32 比特内存访问
   // lsu_type := decoder.io.data_type_o
-  lsu_sign_ext := decoder.io.data_sign_extension_o
+  //  lsu_sign_ext := decoder.io.data_sign_extension_o
 
   jump_in_dec := decoder.io.jump_in_dec_o
   branch_in_dec := decoder.io.branch_in_dec_o
 
 
-
   val controller: ibex_controller = Module(new ibex_controller)
   // todo: clk_i & rst_ni
   io.ctrl_busy_o <> controller.io.ctrl_busy_o
-  io.instr_valid_i <> controller.io.instr_valid_i
-  io.instr_rdata_i <> controller.io.instr_rdata_i
-  io.pc_id_i <> controller.io.pc_id_i
   io.instr_valid_clear_o <> controller.io.instr_valid_clear_o
   io.id_in_ready_o <> controller.io.id_in_ready_o
   controller_run := controller.io.controller_run_o
@@ -317,7 +321,7 @@ class ibex_id_stage extends Module {
   //
   // assign branch_set_raw      = /* (BranchTargetALU && !data_ind_timing_i) ? branch_set_raw_d : */
 
-  branch_jump_set_done_d := (branch_set_raw | jump_set_raw | branch_jump_set_done_q) & ~io.oinstr_valid_clear_o
+  branch_jump_set_done_d := (branch_set_raw | jump_set_raw | branch_jump_set_done_q) & ~io.instr_valid_clear_o
   // always_ff @(posedge clk_i or negedge rst_ni) begin
   //   if (!rst_ni) begin
   //     branch_jump_set_done_q <= 1'b0;
@@ -328,9 +332,25 @@ class ibex_id_stage extends Module {
   jump_set := jump_set_raw & ~branch_set_raw_q
   branch_set := branch_set_raw & ~branch_jump_set_done_q
 
-  // typedef enum logic { FIRST_CYCLE, MULTI_CYCLE } id_fsm_e;
-  // id_fsm_e id_fsm_q, id_fsm_d;
-  //
+  object id_fsm_e extends ChiselEnum {
+    val
+    FIRST_CYCLE,
+    MULTI_CYCLE
+    = Value
+  }
+
+  val id_fsm_q: id_fsm_e.Type = id_fsm_e()
+  val id_fsm_d: id_fsm_e.Type = id_fsm_e()
+
+  val reset_n: AsyncReset = (!reset.asBool).asAsyncReset
+  id_fsm_q := withReset(reset_n) {
+    RegNext(id_fsm_d, init = ctrl_fsm_e.RESET)
+  }
+
+  withReset(reset_n) {
+    id_fsm_q := Mux(instr_executing, RegNext(id_fsm_d, init = id_fsm_e.FIRST_CYCLE), id_fsm_e.FIRST_CYCLE)
+  }
+  // Expect equal to below
   // always_ff @(posedge clk_i or negedge rst_ni) begin : id_pipeline_reg
   //   if (!rst_ni) begin
   //     id_fsm_q <= FIRST_CYCLE;
@@ -338,11 +358,11 @@ class ibex_id_stage extends Module {
   //     id_fsm_q <= id_fsm_d;
   //   end
   // end
-  //
-  // // ID/EX stage can be in two states, FIRST_CYCLE and MULTI_CYCLE. An instruction enters
-  // // MULTI_CYCLE if it requires multiple cycles to complete regardless of stalls and other
-  // // considerations. An instruction may be held in FIRST_CYCLE if it's unable to begin executing
-  // // (this is controlled by instr_executing).
+
+  // ID/EX stage can be in two states, FIRST_CYCLE and MULTI_CYCLE. An instruction enters
+  // MULTI_CYCLE if it requires multiple cycles to complete regardless of stalls and other
+  // considerations. An instruction may be held in FIRST_CYCLE if it's unable to begin executing
+  // (this is controlled by instr_executing).
   //
   // always_comb begin
   //   id_fsm_d                = id_fsm_q;
@@ -409,21 +429,21 @@ class ibex_id_stage extends Module {
   // end
   stall_id := stall_mem | stall_jump | stall_branch | stall_alu
   instr_done := ~stall_id
-  instr_first_cycle := instr_valid_i & (id_fsm_q == id_fsm_e.FIRST_CYCLE)
-  instr_first_cycle_id_o := instr_first_cycle
+  instr_first_cycle := io.instr_valid_i & (id_fsm_q == id_fsm_e.FIRST_CYCLE)
+  io.instr_first_cycle_id_o := instr_first_cycle
 
   // 多周期指令完成：
   // - LSU 操作完成
   // - execute block 完成
-  multicycle_done := Mux(lsu_req_dec, lsu_resp_valid_i, ex_valid_i)
+  multicycle_done := Mux(lsu_req_dec, io.lsu_resp_valid_i, io.ex_valid_i)
   data_req_allowed := instr_first_cycle
-  stall_mem := instr_valid_i & (lsu_req_dec & (~lsu_resp_valid_i | instr_first_cycle))
-  instr_executing := instr_valid_i & controller_run
+  stall_mem := io.instr_valid_i & (lsu_req_dec & (~io.lsu_resp_valid_i | instr_first_cycle))
+  instr_executing := io.instr_valid_i & controller_run
 
   // No data forwarding without writeback stage so always take source register data direct from
   // register file
-  rf_rdata_a_fwd := rf_rdata_a_i;
-  rf_rdata_b_fwd := rf_rdata_b_i;
+  rf_rdata_a_fwd := io.rf_rdata_a_i;
+  rf_rdata_b_fwd := io.rf_rdata_b_i;
 
-  instr_id_done_o := instr_done
+  io.instr_id_done_o := instr_done
 }
