ad863cf613c66df6a369c68146d0c2b578303d93
谭源 <kentertan12138@Outlook.com>
2021-06-08 20:46:24 +0800

    Rewrite id_stage

--- src/main/scala/ibex_id_stage.scala
+++ src/main/scala/ibex_id_stage.scala
@@ -1,5 +1,3 @@
-// 忽略 LSU
-
 import Chisel.{Cat, MuxLookup, is, switch}
 import chisel3._
 import chisel3.experimental.ChiselEnum
@@ -8,32 +6,104 @@ class ibex_id_stage extends Module {
   val io = IO(new Bundle {
     val ctrl_busy_o: Bool = Output(Bool())
 
-    val instr_valid_i: Bool = Input(Bool())
-    val instr_rdata_i: Bool = Input(Bool())
-    val instr_rdata_alu_i: Bool = Input(Bool())
+    // Interface to IF stage
+    val instr_valid_i: Bool = Input(Bool()) // from IF-ID pipeline registers
+    val instr_rdata_i: Bool = Input(Bool()) // from IF-ID pipeline registers
+    val instr_rdata_alu_i: Bool = Input(Bool()) // from IF-ID pipeline registers
     val instr_req_o: Bool = Output(Bool())
     val instr_first_cycle_id_o: Bool = Output(Bool())
-    val instr_valid_clear_o: Bool = Output(Bool())
-    val id_in_ready_o: Bool = Output(Bool())
+    val instr_valid_clear_o: Bool = Output(Bool()) // kill instr in IF-ID reg
+    val id_in_ready_o: Bool = Output(Bool()) // ID stage is ready for next instr
 
+    // Jumps and branches
     val branch_decision_i: Bool = Input(Bool())
 
+    // IF and ID stage signals
     val pc_set_o: Bool = Output(Bool())
     val pc_mux_o: pc_sel_e.Type = Output(pc_sel_e())
 
     val pc_id_i: UInt = Input(UInt(32.W))
 
-    val ex_valid_i: Bool = Input(Bool()) // todo: ALU 输出(总是为 1)
-
-    val lsu_resp_valid_i: Bool = Input(Bool()) // todo: LSU 空闲标志
-    val lsu_req_o: Bool = Output(Bool())
-    val lsu_we_o: Bool = Output(Bool())
-    val lsu_wdata_o: UInt = Output(UInt(32.W))
-
+    // Stalls
+    val ex_valid_i: Bool = Input(Bool()) // EX stage has valid output ALU 输出(总是为 1)
+    val lsu_resp_valid_i: Bool = Input(Bool()) // // LSU has valid output, or is done LSU 空闲标志
+    // ALU
     val alu_operator_ex_o: alu_op_e.Type = Output(alu_op_e())
     val alu_operand_a_ex_o: UInt = Output(UInt(32.W))
     val alu_operand_b_ex_o: UInt = Output(UInt(32.W))
 
+    //    // Multicycle Operation Stage Register
+    //    input  logic [1:0]                imd_val_we_ex_i,
+    //    input  logic [33:0]               imd_val_d_ex_i[2],
+    //    output logic [33:0]               imd_val_q_ex_o[2],
+    //
+    //    // Branch target ALU
+    //    output logic [31:0]               bt_a_operand_o,
+    //    output logic [31:0]               bt_b_operand_o,
+    //
+    //    // MUL, DIV
+    //    output logic                      mult_en_ex_o,
+    //    output logic                      div_en_ex_o,
+    //    output logic                      mult_sel_ex_o,
+    //    output logic                      div_sel_ex_o,
+    //    output ibex_pkg::md_op_e          multdiv_operator_ex_o,
+    //    output logic  [1:0]               multdiv_signed_mode_ex_o,
+    //    output logic [31:0]               multdiv_operand_a_ex_o,
+    //    output logic [31:0]               multdiv_operand_b_ex_o,
+    //    output logic                      multdiv_ready_id_o,
+    //
+    //    // CSR
+    //    output logic                      csr_access_o,
+    //    output ibex_pkg::csr_op_e         csr_op_o,
+    //    output logic                      csr_op_en_o,
+    //    output logic                      csr_save_if_o,
+    //    output logic                      csr_save_id_o,
+    //    output logic                      csr_save_wb_o,
+    //    output logic                      csr_restore_mret_id_o,
+    //    output logic                      csr_restore_dret_id_o,
+    //    output logic                      csr_save_cause_o,
+    //    output logic [31:0]               csr_mtval_o,
+    //    input  ibex_pkg::priv_lvl_e       priv_mode_i,
+    //    input  logic                      csr_mstatus_tw_i,
+    //    input  logic                      illegal_csr_insn_i,
+    //    input  logic                      data_ind_timing_i,
+
+    // Interface to load store unit
+    val lsu_req_o: Bool = Output(Bool())
+    val lsu_we_o: Bool = Output(Bool())
+    val lsu_wdata_o: UInt = Output(UInt(32.W))
+    //    input  logic                      lsu_req_done_i, // Data req to LSU is complete and
+    //    // instruction can move to writeback
+    //    // (only relevant where writeback stage is
+    //    // present)
+    //
+    //    input  logic                      lsu_addr_incr_req_i,
+    //    input  logic [31:0]               lsu_addr_last_i,
+    //
+    //    // Interrupt signals
+    //    input  logic                      csr_mstatus_mie_i,
+    //    input  logic                      irq_pending_i,
+    //    input  ibex_pkg::irqs_t           irqs_i,
+    //    input  logic                      irq_nm_i,
+    //    output logic                      nmi_mode_o,
+    //
+    //    input  logic                      lsu_load_err_i,
+    //    input  logic                      lsu_store_err_i,
+    //
+    //    // Debug Signal
+    //    output logic                      debug_mode_o,
+    //    output ibex_pkg::dbg_cause_e      debug_cause_o,
+    //    output logic                      debug_csr_save_o,
+    //    input  logic                      debug_req_i,
+    //    input  logic                      debug_single_step_i,
+    //    input  logic                      debug_ebreakm_i,
+    //    input  logic                      debug_ebreaku_i,
+    //    input  logic                      trigger_match_i,
+    //
+    val result_ex_i: UInt = Input(UInt(32.W))
+
+
+    // Register file read
     val rf_raddr_a_o: UInt = Output(UInt(5.W))
     val rf_rdata_a_i: UInt = Input(UInt(32.W))
     val rf_raddr_b_o: UInt = Output(UInt(5.W))
@@ -41,9 +111,33 @@ class ibex_id_stage extends Module {
     val rf_ren_a_o: Bool = Output(Bool())
     val rf_ren_b_o: Bool = Output(Bool())
 
+    // Register file write (via writeback)
     val rf_waddr_id_o: UInt = Output(UInt(4.W))
+    val rf_wdata_id_o: UInt = Input(UInt(32.W))
     val rf_we_id_o: Bool = Output(Bool())
-
+    //    output logic                      rf_rd_a_wb_match_o,
+    //    output logic                      rf_rd_b_wb_match_o,
+    //
+    //    // Register write information from writeback (for resolving data hazards)
+    //    input  logic [4:0]                rf_waddr_wb_i,
+    //    input  logic [31:0]               rf_wdata_fwd_wb_i,
+    //    input  logic                      rf_write_wb_i,
+    //
+    //    output  logic                     en_wb_o,
+    //    output  ibex_pkg::wb_instr_type_e instr_type_wb_o,
+    //    output  logic                     instr_perf_count_id_o,
+    //    input logic                       ready_wb_i,
+    //    input logic                       outstanding_load_wb_i,
+    //    input logic                       outstanding_store_wb_i,
+    //
+    //    // Performance Counters
+    //    output logic                      perf_jump_o,    // executing a jump instr
+    //    output logic                      perf_branch_o,  // executing a branch instr
+    //    output logic                      perf_tbranch_o, // executing a taken branch instr
+    //    output logic                      perf_dside_wait_o, // instruction in ID/EX is awaiting memory
+    //    // access to finish before proceeding
+    //    output logic                      perf_mul_wait_o,
+    //    output logic                      perf_div_wait_o,
     val instr_id_done_o: Bool = Output(Bool())
 
   })
@@ -51,25 +145,15 @@ class ibex_id_stage extends Module {
   val branch_in_dec: Bool = Wire(Bool())
 
   val branch_set: Bool = Wire(Bool())
-  val branch_set_raw: Bool = Wire(Bool())
-  val branch_set_raw_d: Bool = Wire(Bool())
-
-  val branch_jump_set_done_q: Bool = Wire(Bool())
-  val branch_jump_set_done_d: Bool = Wire(Bool())
-
-  val branch_taken: Bool = Wire(Bool())
-
+  val branch_set_d: Bool = Wire(Bool())
   val jump_in_dec: Bool = Wire(Bool())
-
   val jump_set_dec: Bool = Wire(Bool())
   val jump_set: Bool = Wire(Bool())
-  val jump_set_raw: Bool = Wire(Bool())
 
   val instr_first_cycle: Bool = Wire(Bool())
   val instr_executing: Bool = Wire(Bool())
   val instr_done: Bool = Wire(Bool())
   val controller_run: Bool = Wire(Bool())
-  val stall_ld_hz: Bool = Wire(Bool())
   val stall_mem: Bool = Wire(Bool())
   val stall_branch: Bool = Wire(Bool())
   val stall_jump: Bool = Wire(Bool())
@@ -84,24 +168,16 @@ class ibex_id_stage extends Module {
   val imm_j_type: UInt = Wire(UInt(32.W))
   val zimm_rs1_type: UInt = Wire(UInt(32.W))
 
-  val imm_a: UInt = Wire(UInt(32.W))
-  val imm_b: UInt = Wire(UInt(32.W))
+  val imm_a: UInt = Wire(UInt(32.W)) // contains the immediate for operand b
+  val imm_b: UInt = Wire(UInt(32.W)) // contains the immediate for operand b
 
   // Register file interface
 
-  val rf_wdata_sel: rf_wd_sel_e.Type = Wire(rf_wd_sel_e())
   val rf_we_dec: Bool = Wire(Bool())
   val rf_we_raw: Bool = Wire(Bool())
 
   val rf_ren_a: Bool = Wire(Bool())
   val rf_ren_b: Bool = Wire(Bool())
-
-  val rf_ren_a_dec: Bool = Wire(Bool())
-  val rf_ren_b_dec: Bool = Wire(Bool())
-
-  // Read enables should only be asserted for valid and legal instructions
-  rf_ren_a := io.instr_valid_i & rf_ren_a_dec
-  rf_ren_b := io.instr_valid_i & rf_ren_b_dec
 
   io.rf_ren_a_o := rf_ren_a
   io.rf_ren_b_o := rf_ren_b
@@ -116,7 +192,6 @@ class ibex_id_stage extends Module {
   val alu_op_b_mux_sel: op_b_sel_e.Type = Wire(op_b_sel_e())
   val alu_op_b_mux_sel_dec: op_b_sel_e.Type = Wire(op_b_sel_e())
   val alu_multicycle_dec: Bool = Wire(Bool())
-  val stall_alu: Bool = Wire(Bool())
 
   val imm_a_mux_sel: imm_a_sel_e.Type = Wire(imm_a_sel_e())
   val imm_b_mux_sel: imm_b_sel_e.Type = Wire(imm_b_sel_e())
@@ -177,6 +252,7 @@ class ibex_id_stage extends Module {
   // Suppress register write if there is an illegal CSR access or instruction is not executing
   io.rf_we_id_o := rf_we_raw & instr_executing
 
+  io.rf_wdata_id_o := io.result_ex_i
 
   /////////////
   // Decoder //
@@ -197,13 +273,13 @@ class ibex_id_stage extends Module {
   imm_u_type := decoder.io.imm_u_type_o
   imm_j_type := decoder.io.imm_j_type_o
 
-  rf_wdata_sel := decoder.io.rf_wdata_sel_o
+  //  rf_wdata_sel := decoder.io.rf_wdata_sel_o
   rf_we_dec := decoder.io.rf_we_o
   io.rf_raddr_a_o := decoder.io.rf_raddr_a_o
   io.rf_raddr_b_o := decoder.io.rf_raddr_b_o
   io.rf_waddr_id_o := decoder.io.rf_waddr_o
-  rf_ren_a_dec := decoder.io.rf_ren_a_o
-  rf_ren_b_dec := decoder.io.rf_ren_b_o
+  rf_ren_a := decoder.io.rf_ren_a_o
+  rf_ren_b := decoder.io.rf_ren_b_o
 
   alu_operator := decoder.io.alu_operator_o
   alu_op_a_mux_sel_dec := decoder.io.alu_op_a_mux_sel_o
@@ -236,7 +312,8 @@ class ibex_id_stage extends Module {
   controller.io.stall_id_i := stall_id
 
 
-  lsu_req := Mux(instr_executing, data_req_allowed & lsu_req_dec, true.B)
+  lsu_req := Mux(instr_executing, data_req_allowed & lsu_req_dec, false.B)
+
   io.lsu_req_o := lsu_req
   io.lsu_we_o := lsu_we
   io.lsu_wdata_o := rf_rdata_b_fwd
@@ -251,32 +328,16 @@ class ibex_id_stage extends Module {
 
   val reset_n: AsyncReset = (!reset.asBool).asAsyncReset
 
-  val branch_set_raw_q: Bool = Wire(Bool())
-  branch_set_raw_q := withReset(reset_n) {
-    RegNext(branch_set_raw_d, init = false.B)
+  val branch_set_q: Bool = Wire(Bool())
+  branch_set_q := withReset(reset_n) {
+    RegNext(branch_set_d, init = false.B)
   }
 
   // Branches always take two cycles in fixed time execution mode, with or without the branch
   // target ALU (to avoid a path from the branch decision into the branch target ALU operand
   // muxing).
-  branch_set_raw := branch_set_raw_q
+  branch_set := branch_set_q
 
-  // Track whether the current instruction in ID/EX has done a branch or jump set.
-  branch_jump_set_done_d := (branch_set_raw | jump_set_raw | branch_jump_set_done_q) & ~io.instr_valid_clear_o
-
-  branch_jump_set_done_q := withReset(reset_n) {
-    RegNext(branch_jump_set_done_d, init = false.B)
-  }
-
-  // the _raw signals from the state machine may be asserted for multiple cycles when
-  // instr_executing_spec is asserted and instr_executing is not asserted. This may occur where
-  // a memory error is seen or a there are outstanding memory accesses (indicate a load or store is
-  // in the WB stage). The branch or jump speculatively begins the fetch but is held back from
-  // completing until it is certain the outstanding access hasn't seen a memory error. This logic
-  // ensures only the first cycle of a branch or jump set is sent to the controller to prevent
-  // needless extra IF flushes and fetches.
-  jump_set := jump_set_raw & ~branch_set_raw_q
-  branch_set := branch_set_raw & ~branch_jump_set_done_q
 
   ///////////////
   // ID-EX FSM //
@@ -289,17 +350,17 @@ class ibex_id_stage extends Module {
     RegNext(id_fsm_d, init = id_fsm_e.FIRST_CYCLE)
   }
 
-  withReset(reset_n) {
-    id_fsm_q := Mux(instr_executing, RegNext(id_fsm_d, init = id_fsm_e.FIRST_CYCLE), id_fsm_e.FIRST_CYCLE)
-  }
-  // Expect equal to below
-  // always_ff @(posedge clk_i or negedge rst_ni) begin : id_pipeline_reg
-  //   if (!rst_ni) begin
-  //     id_fsm_q <= FIRST_CYCLE;
-  //   end else if (instr_executing) begin
-  //     id_fsm_q <= id_fsm_d;
-  //   end
-  // end
+  //  withReset(reset_n) {
+  //    id_fsm_q := Mux(instr_executing, RegNext(id_fsm_d, init = id_fsm_e.FIRST_CYCLE), id_fsm_e.FIRST_CYCLE)
+  //  }
+  //  // Expect equal to below
+  //  // always_ff @(posedge clk_i or negedge rst_ni) begin : id_pipeline_reg
+  //  //   if (!rst_ni) begin
+  //  //     id_fsm_q <= FIRST_CYCLE;
+  //  //   end else if (instr_executing) begin
+  //  //     id_fsm_q <= id_fsm_d;
+  //  //   end
+  //  // end
 
   // ID/EX stage can be in two states, FIRST_CYCLE and MULTI_CYCLE. An instruction enters
   // MULTI_CYCLE if it requires multiple cycles to complete regardless of stalls and other
@@ -311,9 +372,8 @@ class ibex_id_stage extends Module {
     rf_we_raw := rf_we_dec;
     stall_jump := 0.U;
     stall_branch := 0.U;
-    stall_alu := 0.U;
-    branch_set_raw_d := 0.U;
-    jump_set_raw := 0.U;
+    branch_set_d := 0.U;
+    jump_set := 0.U;
 
     when(instr_executing) {
       when(id_fsm_q === id_fsm_e.FIRST_CYCLE) {
@@ -325,23 +385,23 @@ class ibex_id_stage extends Module {
           // condition.
           id_fsm_d := Mux(io.branch_decision_i, id_fsm_e.MULTI_CYCLE, id_fsm_e.FIRST_CYCLE)
           stall_branch := io.branch_decision_i
-          branch_set_raw_d := io.branch_decision_i
+          branch_set_d := io.branch_decision_i
         }.elsewhen(jump_in_dec === true.B) {
           // uncond branch operation
           // BTALU means jumps only need one cycle
           id_fsm_d := id_fsm_e.MULTI_CYCLE
           stall_jump := true.B
           jump_set := jump_set_dec
-        }.elsewhen(alu_multicycle_dec === true.B) {
-          stall_alu := 1.U
-          id_fsm_d := id_fsm_e.MULTI_CYCLE;
-          rf_we_raw := false.B
         }.otherwise {
           id_fsm_d := id_fsm_e.FIRST_CYCLE
         }
       }.elsewhen(id_fsm_q === id_fsm_e.MULTI_CYCLE) {
-        stall_branch := branch_in_dec
-        stall_jump := jump_in_dec
+        when(multicycle_done) {
+          id_fsm_d := id_fsm_e.FIRST_CYCLE
+        }.otherwise {
+          stall_branch := branch_in_dec
+          stall_jump := jump_in_dec
+        }
       }.otherwise {
         id_fsm_d := id_fsm_e.FIRST_CYCLE
       }
@@ -349,9 +409,8 @@ class ibex_id_stage extends Module {
 
   }
 
-  // Stall ID/EX stage for reason that relates to instruction in ID/EX, update assertion below if
-  // modifying this.
-  stall_id := stall_mem | stall_jump | stall_branch | stall_alu
+  // Stall ID/EX stage for reason that relates to instruction in ID/EX
+  stall_id := stall_mem | stall_jump | stall_branch
   instr_done := ~stall_id
   instr_first_cycle := io.instr_valid_i & (id_fsm_q === id_fsm_e.FIRST_CYCLE)
   io.instr_first_cycle_id_o := instr_first_cycle
@@ -364,10 +423,9 @@ class ibex_id_stage extends Module {
 
   // Without Writeback Stage always stall the first cycle of a load/store.
   // Then stall until it is complete
-  stall_mem := io.instr_valid_i & (lsu_req_dec & (~io.lsu_resp_valid_i | instr_first_cycle))
-
-  // No load hazards without Writeback Stage
-  stall_ld_hz := 0.U
+  val tmp: Bool = Wire(Bool())
+  tmp := ~io.lsu_resp_valid_i
+  stall_mem := io.instr_valid_i & (lsu_req_dec & (tmp | instr_first_cycle))
 
   // Without writeback stage any valid instruction that hasn't seen an error will execute
   instr_executing := io.instr_valid_i & controller_run
